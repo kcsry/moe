dfe8ffedbd46aa3d9d3478961982af8b59ca7809
Aarni Koskela <akx@iki.fi>
2015-09-02 18:38:16 +0300

    Simplify ajax

--- src/ajax.js
+++ src/ajax.js
@@ -6,18 +6,18 @@
  */
 
 function ajax(options) {
+    const noop = function() {};
     const client = new XMLHttpRequest();
-    options.success = options.success || function() {};
-    options.error = options.error || function() {};
+    options.success = options.success || noop;
+    options.error = options.error || noop;
     options.async = true;
     client.open(options.method || 'GET', options.url);
     client.send(options.data);
     client.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200) {
-            options.success(this.responseText, this);
-        } else if (this.readyState === 4) {
-            options.error(this.status, this.statusText, this);
-        }
+        if(this.readyState !== 4) return;
+        if (this.status === 200) options.success(this.responseText, this);
+        else options.error(this.status, this.statusText, this);
+        client.onreadystatechange = noop;
     };
     return client;
 }
